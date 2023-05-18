ecb84df6d20c3fd81e0d39d7d51e401b0980ea02
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2023-05-18 14:12:08 -0700

    Added posts to community page, odd scrolling behavior

--- components/pages/CommunityPage.jsx
+++ components/pages/CommunityPage.jsx
@@ -1,20 +1,33 @@
 import { Text, ScrollView, Image, StyleSheet, FlatList, View} from "react-native";
 import TopBar from "../elements/TopBar";
+import PostCard from "../elements/PostCard";
 
-export default function CommunityPage({setLoggedInUser}) {
+const fakePosts = require("../../modelData/posts.json");
+
+export default function CommunityPage({navigation, setLoggedInUser}) {
   return (
     <ScrollView>
       <TopBar setLoggedInUser={setLoggedInUser}></TopBar>
-
       <Image style = {styles.image} source = {require('../../assets/arizona_mom.jpeg')}/>
-      <View style ={styles.card}>
-      <Text style={styles.communityTitle}>
-        arizona
-        <Text style={[styles.communityTitle, styles.boldText]}>moms</Text>
-      </Text>
-      <Text style = {styles.description}> Welcome! arizonamoms is a community for
-      moms located in Arizona. Here are our community norms:
-      </Text>
+
+      <View style ={styles.titlecard}>
+        <Text style={styles.communityTitle}>
+            arizona
+            <Text style={[styles.communityTitle, styles.boldText]}>moms</Text>
+        </Text>
+        <Text style = {styles.description}> Welcome! arizonamoms is a community for
+            moms located in Arizona. Here are our community norms:
+        </Text>
+      </View>
+
+      <View>
+        {fakePosts.map((post, index) => {
+          return (
+            <View key={index}>
+              <PostCard navigation={navigation} postInfo={post}></PostCard>
+            </View>
+          );
+        })}
       </View>
     
     </ScrollView>
@@ -24,12 +37,12 @@ export default function CommunityPage({setLoggedInUser}) {
 const styles = StyleSheet.create({
     image: {
         width: '100%',
-        height: '240%',
+        height: 300,
     },
-    card: {
-        backgroundColor: "#ECECEC",
+    titlecard: {
+        backgroundColor: "#ECECEC", //#ECECEC
         flexDirection: "column",
-        height: "100%",
+        height: 100,
         marginTop: 0,
       },
     communityTitle: {
